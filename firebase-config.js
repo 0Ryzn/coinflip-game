9e080d81d7a4d45d25c8b53b1e598c097b0f24b9
0Ryzn <farrasnur9@gmail.com>
2025-05-17 01:09:17 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqhtQTHz5ce7UlWM1r4UvqVd-DvGyzHfM",
+  apiKey: "AIzaSyCqhtQTHz5ce7ULWM1r4UvqVd-DvGyzHFM",
   authDomain: "coinflip-game-dc89b.firebaseapp.com",
   projectId: "coinflip-game-dc89b",
-  storageBucket: "coinflip-game-dc89b.firebasestorage.app",
+  storageBucket: "coinflip-game-dc89b.appspot.com",
   messagingSenderId: "641730135558",
   appId: "1:641730135558:web:1a886439033df4b5962587",
-  measurementId: "G-MWKG8TQC9B"
+  measurementId: "G-MwKG8TQC98"
 };
+
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
+
